57e53428406216a6dab0909b6a777df9fa80c75e
wpl0923 <wpl0923@live.hk>
2020-12-29 14:53:59 +0000

    AfterPartC

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,30 +1,47 @@
 const express = require('express');
 const app = new express();
+const dotenv = require('dotenv');
+const { IamAuthenticator } = require('ibm-watson/auth');
+const naturalLanguageUndertstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+
+dotenv.config();
+
+function getLanguageTranslator() {
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2020-08-01',
+        authenticator: new IamAuthenticator({
+            apikey: api_key,
+        }),
+        serviceUrl: api_url
+    })
+}
 
 app.use(express.static('client'))
 
 const cors_app = require('cors');
 app.use(cors_app());
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render('index.html');
-  });
-
-app.get("/url/emotion", (req,res) => {
-
-    return res.send({"happy":"90","sad":"10"});
 });
 
-app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
+app.get("/url/emotion", (req, res) => {
+
+    return res.send({ "happy": "90", "sad": "10" });
 });
 
-app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+app.get("/url/sentiment", (req, res) => {
+    return res.send("url sentiment for " + req.query.url);
 });
 
-app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+app.get("/text/emotion", (req, res) => {
+    return res.send({ "happy": "10", "sad": "90" });
+});
+
+app.get("/text/sentiment", (req, res) => {
+    return res.send("text sentiment for " + req.query.text);
 });
 
 let server = app.listen(8080, () => {
